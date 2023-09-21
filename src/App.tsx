c00a6f0de23682d8268cce4b8e071ca81d3728c3
Logan Gray <111297610+loganallengray@users.noreply.github.com>
2023-09-21 17:32:18 -0500

    API Setup (#15)
    * data added to json file
    
    * json api set up
    
    * readme updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import AppRoutes from './components/AppRoutes';
 import Header from './components/header/Header';
@@ -9,6 +9,13 @@ const appContextProps = {
 };
 
 const App = () => {
+	// fetches list of users
+	// useEffect(() => {
+	// 	fetch('http://localhost:8088/users')
+	// 		.then(response => response.text())
+	// 		.then(text => console.log(text))
+	// }, [])
+
 	return (
 		<div className="app">
 			<AppContext.Provider value={appContextProps}>
