cc71ff7da62c54d11610ae583b35456b0e30af63
Logan Gray <loganallengray@gmail.com>
2023-09-26 11:13:01 -0500

    navbar updated

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,24 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
+    const location = useLocation();
+    const currentLocation = location.pathname
+
     return (
         <div id="header">
             <Link
-                className="header-link-item"
+                className={"header-link-item" + `${currentLocation === "/" ? " " + "header-selected-item" : ""}`}
                 to="/">
                 <p>
                     SoupScript
                 </p>
             </Link>
             <Link
-                className="header-link-item"
+                className={"header-link-item" + `${currentLocation === "/menu" ? " " + "header-selected-item" : ""}`}
                 to="/menu">
                 <p>
                     Menu
                 </p>
             </Link>
             <Link
-                className="header-link-item"
+                className={"header-link-item" + `${currentLocation === "/cart" ? " " + "header-selected-item" : ""}`}
                 to="/cart">
                 <p>
                     Cart
