cbcea70039eda1af4c674eb2b0526c05de220afa
Logan Gray <loganallengray@gmail.com>
2023-10-02 20:51:54 -0500

    cart added to context

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import AppRoutes from './components/AppRoutes';
 import Header from './components/header/Header';
 import AppContext from './modules/context/app-context';
-
-const appContextProps = {
-	test: "test"
-};
+import { ISoup } from './modules/interfaces';
 
 const App = () => {
-	// fetches list of users
-	// useEffect(() => {
-	// 	fetch('http://localhost:8088/users')
-	// 		.then(response => response.text())
-	// 		.then(text => console.log(text))
-	// }, [])
+	const [cart, setCart] = useState<ISoup[] | []>([]);
+
+	const addCartItem = (soup: ISoup) => {
+		const cartCopy = [...cart];
+
+		cartCopy.push(soup)
+
+		setCart(cartCopy);
+	}
+
+	const removeCartItem = (index: number) => {
+		const cartCopy = [...cart];
+
+		const newCart = cartCopy.splice(index, 1);
+
+		setCart(newCart);
+	}
+
+	const appContextProps = {
+		cart: cart,
+		addCartItem: addCartItem,
+		removeCartItem: removeCartItem,
+	};
+
+	console.log(cart);
 
 	return (
 		<div className="app">
